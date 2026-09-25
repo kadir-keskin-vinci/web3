5afaedf3db1ee756f2195f9b427e136be48ea111
kadir-keskin-vinci <kadir.keskin@student.vinci.be>
2026-09-25 09:06:40 +0200

    first commit

---
+++ backend/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import expensesRouter from './routes/expenses.router.ts';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors({ origin: [/localhost/, /\.onrender\.com$/] }));
+
+app.get('/ping', (req, res) => {
+  res.sendStatus(204);
+});
+
+app.use('/api/expenses', expensesRouter);
+
+app.listen(3000, () => {
+  console.log('Server listening on http://localhost:3000');
+});
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', /\.onrender\.com$/],
+  })
+);
+
+export default app;
